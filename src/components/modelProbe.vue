693d434c8af9957c125fc5ad0c31cd609b783175
dobutamine <tim.antonius@outlook.com>
2022-09-27 22:14:59 +0200

    improvs

--- src/components/modelProbe.vue
+++ src/components/modelProbe.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
-    <div class="chart" :id="chartId"></div>
-    <div class="row q-mt-sm">
+    <div class="row q-mt-md">
       <div class="col">
-        <div class="q-gutter-es row gutter justify-center">
+        <div class="q-gutter-md row justify-center">
           <q-select
             label-color="red-6"
             v-model="selected_component_name1"
             :options="component_names"
             hide-bottom-space
-            filled
+            outlined
             dense
             label="y1"
             style="width: 100px; font-size: 12px"
@@ -21,7 +20,7 @@
             v-model="selected_prim_prop_name1"
             :options="prim_prop_names1"
             hide-bottom-space
-            filled
+            outlined
             dense
             label="prop1"
             style="width: 100px; font-size: 12px"
@@ -33,7 +32,7 @@
             v-model="selected_sec_prop_name1"
             :options="sec_prop_names1"
             hide-bottom-space
-            filled
+            outlined
             dense
             square
             label="prop2"
@@ -45,7 +44,7 @@
             v-model="selected_component_name2"
             :options="component_names"
             hide-bottom-space
-            filled
+            outlined
             dense
             square
             label="y2"
@@ -59,7 +58,7 @@
             v-model="selected_prim_prop_name2"
             :options="prim_prop_names2"
             hide-bottom-space
-            filled
+            outlined
             dense
             square
             label="prop1"
@@ -72,7 +71,7 @@
             v-model="selected_sec_prop_name2"
             :options="sec_prop_names2"
             hide-bottom-space
-            filled
+            outlined
             dense
             square
             label="prop2"
@@ -85,7 +84,7 @@
             v-model="selected_component_name3"
             :options="component_names"
             hide-bottom-space
-            filled
+            outlined
             dense
             square
             label="y3"
@@ -99,7 +98,7 @@
             v-model="selected_prim_prop_name3"
             :options="prim_prop_names3"
             hide-bottom-space
-            filled
+            outlined
             dense
             square
             label="prop1"
@@ -112,7 +111,7 @@
             v-model="selected_sec_prop_name3"
             :options="sec_prop_names3"
             hide-bottom-space
-            filled
+            outlined
             dense
             square
             label="prop2"
@@ -122,9 +121,11 @@
       </div>
     </div>
 
-    <div class="row">
+    <div class="chart" :id="chartId"></div>
+
+    <div class="row q-mt-sm">
       <div class="col">
-        <div class="q-gutter-lg row gutter q-ma-xs justify-center">
+        <div class="q-gutter-sm row justify-center">
           <q-checkbox
             v-model="show_summary"
             dense
@@ -178,7 +179,7 @@
             v-model.number="chart1_factor"
             type="number"
             label="y1"
-            filled
+            outlined
             dense
             style="width: 75px; font-size: 10px"
           />
@@ -187,7 +188,16 @@
             v-model.number="chart2_factor"
             type="number"
             label="y2"
-            filled
+            outlined
+            dense
+            style="width: 75px; font-size: 10px"
+          />
+          <q-input
+            v-if="scaling"
+            v-model.number="chart3_factor"
+            type="number"
+            label="y3"
+            outlined
             dense
             style="width: 75px; font-size: 10px"
           />
@@ -195,12 +205,14 @@
             v-model.number="number_of_seconds"
             type="number"
             label="frame(s)"
-            filled
+            outlined
             dense
             style="width: 75px; font-size: 10px"
           />
           <div class="row">
-            <q-btn outline size="sm" @click="calculate">CALCULATE</q-btn>
+            <q-btn color="negative" size="sm" @click="calculate"
+              >CALCULATE</q-btn
+            >
           </div>
         </div>
       </div>
@@ -243,6 +255,7 @@ export default {
       scaling: false,
       chart1_factor: 1.0,
       chart2_factor: 1.0,
+      chart3_factor: 1.0,
       rt_frame: 1.0,
       number_of_seconds: 5,
       hiresLogging: false,
@@ -288,8 +301,8 @@ export default {
       autoscaleY: false,
       title: "Title",
       lineTitle: "LineTitle",
-      yLabel: "parameter",
-      xLabel: "time",
+      yLabel: "",
+      xLabel: "time(s)",
       width: "100%",
       height: "300px",
       first_run: true,
@@ -532,12 +545,15 @@ export default {
     dataUpdate() {
       this.chartData1 = [];
       this.chartData2 = [];
+      this.chartData3 = [];
       this.lineSeries1.clear();
       this.lineSeries2.clear();
+      this.lineSeries3.clear();
 
       if (!this.scaling) {
         this.chart1_factor = 1.0;
         this.chart2_factor = 1.0;
+        this.chart3_factor = 1.0;
       }
 
       let prop1 = "";
@@ -574,6 +590,23 @@ export default {
         }
       }
 
+      let prop3 = "";
+      let postFix3 = "";
+      let chart3_enabled = false;
+      if (this.selected_component_name3 && this.selected_prim_prop_name3) {
+        chart3_enabled = true;
+        prop3 =
+          this.selected_component_name3 + "." + this.selected_prim_prop_name3;
+        if (this.selected_sec_prop_name3) {
+          prop3 += "." + this.selected_sec_prop_name3;
+        }
+
+        postFix3 = "";
+        if (this.selected_prim_prop_name2 === "compounds") {
+          postFix3 = "conc";
+        }
+      }
+
       explainModel.modelData.forEach((data) => {
         if (chart1_enabled) {
           let y1 = parseFloat(data[prop1]) * this.chart1_factor;
@@ -598,6 +631,18 @@ export default {
             y: y2,
           });
         }
+
+        if (chart3_enabled) {
+          let y3 = parseFloat(data[prop3]) * this.chart3_factor;
+          if (postFix3) {
+            y3 = parseFloat(data[prop3][postFix3]) * this.chart3_factor;
+          }
+
+          this.chartData3.push({
+            x: data.time,
+            y: y3,
+          });
+        }
       });
 
       if (chart1_enabled) {
@@ -606,12 +651,15 @@ export default {
       if (chart2_enabled) {
         this.lineSeries2.add(this.chartData2);
       }
+      if (chart3_enabled) {
+        this.lineSeries3.add(this.chartData3);
+      }
     },
     statusUpdate() {
       console.log(explainModel.statusMessage);
     },
     calculate() {
-      if (this.selectComponent1 && this.selected_prim_prop_name1) {
+      if (this.selected_component_name1 && this.selected_prim_prop_name1) {
         explainModel.watchModelProperty(
           this.selected_component_name1,
           this.selected_prim_prop_name1,
@@ -619,13 +667,22 @@ export default {
         );
       }
 
-      if (this.selectComponent2 && this.selected_prim_prop_name2) {
+      if (this.selected_component_name2 && this.selected_prim_prop_name2) {
         explainModel.watchModelProperty(
           this.selected_component_name2,
           this.selected_prim_prop_name2,
           this.selected_sec_prop_name2
         );
       }
+
+      if (this.selected_component_name3 && this.selected_prim_prop_name3) {
+        explainModel.watchModelProperty(
+          this.selected_component_name3,
+          this.selected_prim_prop_name3,
+          this.selected_sec_prop_name3
+        );
+      }
+
       explainModel.calculate(parseInt(this.number_of_seconds));
       if (this.first_run) {
         explainModel.getModelState();
@@ -649,11 +706,17 @@ export default {
         maintainAspectRatio: false,
       });
 
-      chart_object.chart.setTitle(this.title);
-      chart_object.chart.setTitleFont(
-        new FontSettings({ size: 12, style: "normal" })
-      );
-      chart_object.chart.setPadding({ top: 0, bottom: 0, left: 15, right: 30 });
+      // chart_object.chart.setTitle(this.title);
+      // chart_object.chart.setTitleFont(
+      //   new FontSettings({ size: 12, style: "normal" })
+      // );
+      chart_object.chart.setTitleFillStyle(emptyFill);
+      chart_object.chart.setPadding({
+        top: 10,
+        bottom: 0,
+        left: 15,
+        right: 30,
+      });
 
       // Set up axes
       chart_object.chartXAxis = chart_object.chart.getDefaultAxisX();
@@ -716,6 +779,14 @@ export default {
       this.lineSeries2.setStrokeStyle((style) => style.setThickness(2));
       this.lineSeries2.setStrokeStyle((style) =>
         style.setFillStyle(new SolidFill({ color: ColorRGBA(0, 200, 0) }))
+      );
+
+      this.lineSeries3 = chart_object.chart
+        .addLineSeries()
+        .setName(this.lineTitle);
+      this.lineSeries3.setStrokeStyle((style) => style.setThickness(2));
+      this.lineSeries3.setStrokeStyle((style) =>
+        style.setFillStyle(new SolidFill({ color: ColorRGBA(0, 0, 200) }))
       );
       //this.lineSeries.add(this.points);
 
